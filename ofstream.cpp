9d730bfa8014f35bc963a1f3abdc11b37a4cd792
titumiranan123 <titumiranan.gtc@gmail.com>
2023-01-02 21:27:43 +0600

    my first commit

---
+++ ofstream.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<fstream>
+
+using namespace std;
+int main(){
+//     ofstream of;
+     
+//     of.open("1.txt");
+//     of<<"Hello world\n";
+//     ifstream in;
+//     in.open("inpt.txt");
+//     string s;
+//     in>>s;
+//     cout<<s;
+//     return 0;
+// of.close();
+// in.close();
+
+
+// string s;
+char s[12];
+cin.getline(s,12);
+cout <<s;
+return 0;
+}
---
+++ programe1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    freopen("inpt.txt", "r", stdin);
+    freopen("out.txt","w",stdout);
+    string s;
+    // cin >> s;
+    getline(cin, s);
+    cout << s << endl;
+    return 0;
+}
